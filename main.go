ef3075a3f38c1318ec7e27f76244ee24dbcce145
tauraamui <aplewis95@gmail.com>
2018-06-26 12:48:05 +0100

    set logging output to not include arrow suffix

--- main.go
+++ main.go
@@ -63,7 +63,7 @@ func runTool(toolFlag string) {
 		outputType := flag.String("ot", "", "Extension of image type to output to.")
 		recursive := flag.Bool("rs", false, "Scan all sub folders in root recursively.")
 		setLoggingLevel()
-		logging.OutputDateTime, logging.OutputPath = false, false
+		logging.OutputDateTime, logging.OutputPath, logging.OutputLogLevelFlag, logging.OutputArrowSuffix = false, false, false, false
 		flag.Parse()
 
 		cltools.RunRtc(*sourceDirectory, *outputDirectory, *inputType, *outputType, *recursive)
