cdc3e73cddc82324a89eafa1a32d5852959e73ec
tauraamui <aplewis95@gmail.com>
2018-07-10 17:26:27 +0100

    add windows script to build cross platform

---
+++ build.bat
@@ -0,0 +1,9 @@
+@echo off
+set VERSION=0.0.2a
+set GOOS_ENVS=(windows linux darwin)
+set GOARCHS=(386 amd64)
+for %%i in %GOOS_ENVS% do (
+	for %%j in %GOARCHS% do (
+		env GOOS=%%i GOARCH=%%j go build -o "bin\clover_v%VERSION%_%%i_%%j\clover"
+	)
+)
