fa94580386dc5ad40fd7d2e7a901f56338f08d46
tauraamui <aplewis95@gmail.com>
2018-07-11 09:04:46 +0100

    update build script and gitignore

--- build.bat
+++ build.bat
@@ -4,6 +4,6 @@ set GOOS_ENVS=(windows linux darwin)
 set GOARCHS=(386 amd64)
 for %%i in %GOOS_ENVS% do (
 	for %%j in %GOARCHS% do (
-		env GOOS=%%i GOARCH=%%j go build -o "bin\clover_v%VERSION%_%%i_%%j\clover"
+		env GOOS=%%i GOARCH=%%j go build -o "bin\clover-v%VERSION%-%%i-%%j\clover"
 	)
 )
