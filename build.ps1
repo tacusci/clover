169db38af740c0fabdfcfa4244e39d6ec44dcb0c
tauraamui <aplewis95@gmail.com>
2018-07-11 12:19:42 +0100

    update build script to support linux arm and freebsd

--- build.ps1
+++ build.ps1
@@ -1,16 +1,17 @@
 
 $VERSION="0.0.2a"
-$GOOS_ENVS=@("windows", "linux", "darwin")
-$GOARCHS=@("386", "amd64")
+$GOOS_ENVS=@("windows", "linux", "darwin", "freebsd")
+$GOARCHS=@("386", "amd64", "arm")
 $WINEXT=""
 
 foreach($i in $GOOS_ENVS) {
     if ($i -eq "windows") { $WINEXT = ".exe" } else { $WINEXT = "" }
     foreach($j in $GOARCHS) {
+        if ($j -eq "arm" -and $i -ne "linux") { continue }
         $OUTPUTFOLDER = "clover-v$VERSION-$i-$j"
-        $OUTPUTPATH = "bin\$OUTPUTFOLDER\clover$WINEXT"
+        $OUTPUTPATH = "bin\v$VERSION\$OUTPUTFOLDER\clover$WINEXT"
         $BUILDSTR = "env GOOS=$i GOARCH=$j go build -o $OUTPUTPATH"
         Invoke-Expression "& $BUILDSTR"
-        Compress-Archive -Path $OUTPUTPATH -DestinationPath "bin\$OUTPUTFOLDER.zip"
+        Compress-Archive -Path $OUTPUTPATH -DestinationPath "bin\v$VERSION\$OUTPUTFOLDER.zip"
     }
 }
